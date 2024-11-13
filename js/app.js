eb28d4a4b5088e6c9e830610a3ace7c5d60c11df
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-13 00:08:50 -0300

    Leer los datos del curso que seleccionamos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const $ = selector => document.querySelector(selector);
 const $$ = selector => document.querySelectorAll(selector);
 const $$$ = (element, event, handler) => element.addEventListener(event, handler);
+const $$$$ = (padre, hijo) => padre.querySelector(hijo);
 
 const carrito = $('#carrito');
 const contenedorCarrito = $('#lista-carrito tbody');
@@ -13,8 +14,30 @@ function cargarEventListeners() {
 }
 
 function agregarCurso(e) {
-    e.preventDefault();
+    e.preventDefault(); // Prevenir evento por defecto
     if(e.target.classList.contains('agregar-carrito')) {
-        console.log(e.target);
+        const cursoSeleccionado = e.target.parentElement.parentElement; // Voy al elemento padre del elemento padre del evento
+        leerDatosCurso(cursoSeleccionado);
+        /*
+        nextElementSibling: Obtiene el siguiente hermano elemento del actual.
+        previousElementSibling: Obtiene el hermano elemento anterior del actual.
+        */
     }
+}
+// Lee el contenido del HTML al que le dimos click y extrae la información del curso
+function leerDatosCurso(curso) {
+
+    // Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: ($$$$(curso, 'img')).src, 
+        titulo: ($$$$(curso, 'h4')).textContent, 
+        precio: ($$$$(curso, '.precio span')).textContent, 
+        id: ($$$$(curso, 'a')).getAttribute('data-id'), 
+        cantidad: 1
+    }
+
+
+    console.log(infoCurso);
+    
+     
 }
